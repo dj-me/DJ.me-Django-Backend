34e3d13ec2f6831e787a90a5122fe712a822894d
vishrutkohli <kohlivishrut@gmail.com>
2017-03-25 00:22:32 +0530

    a

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+import urllib2
+from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+import json
+import requests
+from reportlab.pdfgen import canvas
+from reportlab.lib import colors
+from reportlab.platypus import Image
+from django.http import HttpResponse
+import urllib2
+from django.utils.decorators import method_decorator
+import json
+import requests
+
 
 # Create your views here.
+
+def home(request):
+	return HttpResponse('hello')
