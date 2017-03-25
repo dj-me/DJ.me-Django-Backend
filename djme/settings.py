25a11e8e02ff3bff170227437860acd70d9ecf30
vishrutkohli <kohlivishrut@gmail.com>
2017-03-25 20:04:27 +0530

    aa

--- djme/settings.py
+++ djme/settings.py
@@ -59,11 +59,14 @@ WSGI_APPLICATION = 'djme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djme',
+        'USER': 'clash',
+        'PASSWORD': 'Awesomegamer123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
