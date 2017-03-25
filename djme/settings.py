72d1cfaa50d1e2eca693981634dea21a4790ba70
vishrutkohli <kohlivishrut@gmail.com>
2017-03-25 20:09:28 +0530

    aa

--- djme/settings.py
+++ djme/settings.py
@@ -71,6 +71,7 @@ DATABASES = {
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+#aaaa
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
