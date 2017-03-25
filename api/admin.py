7ee662bf860e4e0076a0d0890b37773ae0b914fd
vishrutkohli <kohlivishrut@gmail.com>
2017-03-25 19:47:12 +0530

    aa

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from api.models import user , hostsong , djsessions , finalplaylist 
+
+
+admin.site.register(user)
+admin.site.register(hostsong)
+admin.site.register(djsessions)
+admin.site.register(finalplaylist)
---
+++ api/migrations/0001_initial.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='djsessions',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('member', models.CharField(default=b'NULL', max_length=250)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='finalplaylist',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('hostedsession', models.CharField(default=b'NULL', max_length=250)),
+                ('counter', models.CharField(default=b'NULL', max_length=250)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='hostsong',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('song', models.CharField(default=b'NULL', max_length=250)),
+                ('counter', models.CharField(default=b'NULL', max_length=250)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='user',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('hostname', models.CharField(default=b'NULL', max_length=250)),
+                ('hotspot', models.CharField(default=b'NULL', max_length=250)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='hostsong',
+            name='hostname',
+            field=models.ForeignKey(to='api.user'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='finalplaylist',
+            name='hostname',
+            field=models.ForeignKey(to='api.user'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='djsessions',
+            name='hostname',
+            field=models.ForeignKey(to='api.user'),
+            preserve_default=True,
+        ),
+    ]
