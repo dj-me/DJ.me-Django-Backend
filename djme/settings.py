5e48171954203002562249fd2548d9f18d3bd2ea
vishrutkohli <kohlivishrut@gmail.com>
2017-03-26 03:59:52 +0530

    aa

--- djme/settings.py
+++ djme/settings.py
@@ -60,9 +60,9 @@ WSGI_APPLICATION = 'djme.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hacks',
-        'USER': 'vish',
-        'PASSWORD': 'password',
+        'NAME': 'clash',
+        'USER': 'vishu',
+        'PASSWORD': 'Awesomegamer123',
         'HOST': 'localhost',
         'PORT': '',
     }
